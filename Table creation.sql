ff77d5fa37d0699ab44b320764ad2faf31e69755
Petro P.H. Horokh <petro.horokh@DEV.COM.UA>
2024-03-12 13:42:52 +0200

    added table Impost for saving data conserning bills and payments. Added table Payment. Created temporal tables for Price and Impost tables

--- Table creation.sql
+++ Table creation.sql
@@ -4,10 +4,10 @@ CREATE TABLE [dbo].[Address](
     [City] [nvarchar](255) NOT NULL,
     [Street] [nvarchar](255) NOT NULL,
     [Building] [nvarchar](255) NOT NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
 	CONSTRAINT PK_Address PRIMARY KEY ([AddressId])
 )
 GO
@@ -15,11 +15,11 @@ GO
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='RoomType' and xtype='U')
 CREATE TABLE [dbo].[RoomType](
     [RoomTypeId] [int] NOT NULL,
-    [Type] [nvarchar](20)  NOT NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
+    [Name] [nvarchar](20)  NOT NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
     CONSTRAINT PK_RoomType PRIMARY KEY ([RoomTypeId])
 )
 GO
@@ -28,14 +28,22 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Price' and xtype='U')
 CREATE TABLE [dbo].[Price](
     [PriceId] [uniqueidentifier] NOT NULL,
     [StartDate] [date] NOT NULL,
+    [Value] [decimal](5,2) NOT NULL,
     [EndDate] [date] NULL,
     [RoomTypeId] [int] NOT NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
-    CONSTRAINT PK_Price PRIMARY KEY ([PriceId])
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
+	[ValidFrom] datetime2 GENERATED ALWAYS AS ROW START HIDDEN NOT NULL,
+    [ValidTo] datetime2 GENERATED ALWAYS AS ROW END HIDDEN NOT NULL,
+    PERIOD FOR SYSTEM_TIME ([ValidFrom], [ValidTo]),
+	CONSTRAINT PK_Price PRIMARY KEY ([PriceId]),
 )
+WITH
+(
+    SYSTEM_VERSIONING = ON (HISTORY_TABLE = [dbo].[Price_History])
+);
 GO
 IF NOT EXISTS(
     SELECT 1
@@ -49,13 +57,14 @@ GO
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Room' and xtype='U')
 CREATE TABLE [dbo].[Room](
     [RoomId] [uniqueidentifier] NOT NULL,
-    [Area] [int]  NOT NULL,
+    [Number] [int] NOT NULL,
+    [Area] [numeric](18, 2)  NOT NULL,
     [RoomTypeId] [int] NOT NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
-    CONSTRAINT PK_Price PRIMARY KEY ([RoomId])
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
+    CONSTRAINT [PK_Room] PRIMARY KEY ([RoomId])
 )
 GO
 IF NOT EXISTS(
@@ -72,10 +81,10 @@ CREATE TABLE [dbo].[Owner](
     [OwnerId] [uniqueidentifier] NOT NULL,
     [Name] [nvarchar](50) NOT NULL,
     [AddressId] [uniqueidentifier] NOT NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
     CONSTRAINT [PK_Owner] PRIMARY KEY ([OwnerId])
 )
 GO
@@ -88,34 +97,32 @@ ALTER TABLE [dbo].[Owner]  WITH CHECK ADD  CONSTRAINT [FK_Owner_AddressId_Addres
 REFERENCES [dbo].[Address] ([AddressId])
 GO
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='OwnerShip' and xtype='U')
-CREATE TABLE [dbo].[OwnerShip](
-    [OwnerShipId] [uniqueidentifier] NOT NULL,
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Asset' and xtype='U')
+CREATE TABLE [dbo].[Asset](
+    [AssetId] [uniqueidentifier] NOT NULL,
     [OwnerId] [uniqueidentifier] NOT NULL,
     [RoomId] [uniqueidentifier] NOT NULL,
-    [StartDate] [date] NOT NULL,
-    [EndDate] [date] NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
-    CONSTRAINT [PK_OwnerShip] PRIMARY KEY([OwnerShipId])
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
+    CONSTRAINT [PK_OwnerShip] PRIMARY KEY([AssetId])
 )
 GO
 IF NOT EXISTS(
     SELECT 1
     FROM sys.foreign_keys
-    WHERE name = 'FK_OwnerShip_OwnerId_Owner_OwnerId'
+    WHERE name = 'FK_Asset_OwnerId_Owner_OwnerId'
 )
-ALTER TABLE [dbo].[OwnerShip]  WITH CHECK ADD  CONSTRAINT [FK_OwnerShip_OwnerId_Owner_OwnerId] FOREIGN KEY([OwnerId])
+ALTER TABLE [dbo].[Asset]  WITH CHECK ADD  CONSTRAINT [FK_Asset_OwnerId_Owner_OwnerId] FOREIGN KEY([OwnerId])
 REFERENCES [dbo].[Owner] ([OwnerId])
 GO
 IF NOT EXISTS(
     SELECT 1
     FROM sys.foreign_keys
-    WHERE name = 'FK_OwnerShip_RoomId_Room_RoomId'
+    WHERE name = 'FK_Asset_RoomId_Room_RoomId'
 )
-ALTER TABLE [dbo].[OwnerShip]  WITH CHECK ADD  CONSTRAINT [FK_OwnerShip_RoomId_Room_RoomId] FOREIGN KEY([RoomId])
+ALTER TABLE [dbo].[Asset]  WITH CHECK ADD  CONSTRAINT [FK_Asset_RoomId_Room_RoomId] FOREIGN KEY([RoomId])
 REFERENCES [dbo].[Room] ([RoomId])
 GO
 
@@ -127,10 +134,10 @@ CREATE TABLE [dbo].[Tenant](
     [AddressId] [uniqueidentifier] NOT NULL,
     [Director] [nvarchar](50) NOT NULL,
     [Description] [nvarchar](255) NOT NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
     CONSTRAINT [PK_Tenant] PRIMARY KEY([TenantId])
 )
 GO
@@ -143,50 +150,59 @@ ALTER TABLE [dbo].[Tenant]  WITH CHECK ADD  CONSTRAINT [FK_Tenant_AddressId_Addr
 REFERENCES [dbo].[Address] ([AddressId])
 GO
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Account' and xtype='U')
-CREATE TABLE [dbo].[Account](
-    [AccountId] [uniqueidentifier] NOT NULL,
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Bill' and xtype='U')
+CREATE TABLE [dbo].[Bill](
+    [BillId] [uniqueidentifier] NOT NULL,
     [TenantId] [uniqueidentifier] NOT NULL,
-    [ToBePaidAmount] [numeric](18, 2) NOT NULL,
-    [StartDate] [date] NOT NULL,
-	[EndDate] [date] NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
-    CONSTRAINT [PK_Account] PRIMARY KEY ([AccountId])
+    [AssetId] [uniqueidentifier] NOT NULL,
+    [BillAmount] [numeric](18, 2) NOT NULL,
+    [IssueDate] [date] NOT NULL,
+    [EndDate] [date] NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
+    CONSTRAINT [PK_Bill] PRIMARY KEY ([BillId])
 )
 GO
 IF NOT EXISTS(
     SELECT 1
     FROM sys.foreign_keys
-    WHERE name = 'FK_Account_TenantId_Tenant_TenantId'
+    WHERE name = 'FK_Bill_TenantId_Tenant_TenantId'
 )
-ALTER TABLE [dbo].[Account]  WITH CHECK ADD  CONSTRAINT [FK_Account_TenantId_Tenant_TenantId] FOREIGN KEY([TenantId])
+ALTER TABLE [dbo].[Bill]  WITH CHECK ADD  CONSTRAINT [FK_Bill_TenantId_Tenant_TenantId] FOREIGN KEY([TenantId])
 REFERENCES [dbo].[Tenant] ([TenantId])
+GO
+IF NOT EXISTS(
+    SELECT 1
+    FROM sys.foreign_keys
+    WHERE name = 'FK_Bill_AssetId_Asset_AssetId'
+)
+ALTER TABLE [dbo].[Bill]  WITH CHECK ADD  CONSTRAINT [FK_Bill_AssetId_Asset_AssetId] FOREIGN KEY([AssetId])
+REFERENCES [dbo].[Asset] ([AssetId])
 GO
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Rent' and xtype='U')
 CREATE TABLE [dbo].[Rent](
-	[RentId] [uniqueidentifier] NOT NULL,
-    [RoomId] [uniqueidentifier] NOT NULL,
+    [RentId] [uniqueidentifier] NOT NULL,
+    [AssetId] [uniqueidentifier] NOT NULL,
     [TenantId] [uniqueidentifier] NOT NULL,
     [StartDate] [date] NOT NULL,
     [EndDate] [date] NULL,
-	[CreatedBy] [uniqueidentifier] NOT NULL,
-	[CreatedDateTime] [datetime] NOT NULL,
-	[ModifiedBy] [uniqueidentifier] NULL,
-	[ModifiedDateTime] [datetime] NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime] NULL,
     CONSTRAINT [PK_Rent] PRIMARY KEY ([RentId])
 )
 GO
 IF NOT EXISTS(
     SELECT 1
     FROM sys.foreign_keys
-    WHERE name = 'FK_Rent_RoomId_Room_RoomId'
+    WHERE name = 'FK_Rent_AssetId_Asset_AssetId'
 )
-ALTER TABLE [dbo].[Rent]  WITH CHECK ADD  CONSTRAINT [FK_Rent_RoomId_Room_RoomId] FOREIGN KEY([RoomId])
-REFERENCES [dbo].[Room] ([RoomId])
+ALTER TABLE [dbo].[Rent]  WITH CHECK ADD  CONSTRAINT [FK_Rent_AssetId_Asset_AssetId] FOREIGN KEY([AssetId])
+REFERENCES [dbo].[Asset] ([AssetId])
 GO
 IF NOT EXISTS(
     SELECT 1
@@ -194,5 +210,58 @@ IF NOT EXISTS(
     WHERE name = 'FK_Rent_TenantId_Tenant_TenantId'
 )
 ALTER TABLE [dbo].[Rent]  WITH CHECK ADD  CONSTRAINT [FK_Rent_TenantId_Tenant_TenantId] FOREIGN KEY([TenantId])
+REFERENCES [dbo].[Tenant] ([TenantId])
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Impost' and xtype='U')
+CREATE TABLE [dbo].[Impost](
+    [ImpostId] [uniqueidentifier] NOT NULL,
+    [Tax] [decimal](4,2) NOT NULL,
+    [Fine] [decimal](3,2) NOT NULL,
+    [PaymentDate] [int] NOT NULL,
+    [StartDate] [date] NOT NULL,
+    [EndDate] [date] NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime2] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime2] NULL,
+	[ValidFrom] datetime2 GENERATED ALWAYS AS ROW START HIDDEN NOT NULL,
+    [ValidTo] datetime2 GENERATED ALWAYS AS ROW END HIDDEN NOT NULL,
+    PERIOD FOR SYSTEM_TIME ([ValidFrom], [ValidTo]),
+    CONSTRAINT [PK_Impost] PRIMARY KEY ([ImpostId])
+)
+WITH
+(
+    SYSTEM_VERSIONING = ON (HISTORY_TABLE = [dbo].[Impost_History])
+);
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Payment' and xtype='U')
+CREATE TABLE [dbo].[Payment](
+    [PaymentId] [uniqueidentifier] NOT NULL,
+    [TenantId] [uniqueidentifier] NOT NULL,
+    [BillId] [uniqueidentifier] NOT NULL,
+    [PaymentDate] [datetime2] NOT NULL,
+    [CreatedBy] [uniqueidentifier] NOT NULL,
+    [CreatedDateTime] [datetime2] NOT NULL,
+    [ModifiedBy] [uniqueidentifier] NULL,
+    [ModifiedDateTime] [datetime2] NULL,
+    CONSTRAINT [PK_Payment] PRIMARY KEY ([PaymentId])
+)
+GO
+IF NOT EXISTS(
+    SELECT 1
+    FROM sys.foreign_keys
+    WHERE name = 'FK_Payment_BillId_Bill_BillId'
+)
+ALTER TABLE [dbo].[Payment]  WITH CHECK ADD  CONSTRAINT [FK_Payment_BillId_Bill_BillId] FOREIGN KEY([BillId])
+REFERENCES [dbo].[Bill] ([BillId])
+GO
+IF NOT EXISTS(
+    SELECT 1
+    FROM sys.foreign_keys
+    WHERE name = 'FK_Payment_TenantId_Tenant_TenantId'
+)
+ALTER TABLE [dbo].[Payment]  WITH CHECK ADD  CONSTRAINT [FK_Payment_TenantId_Tenant_TenantId] FOREIGN KEY([TenantId])
 REFERENCES [dbo].[Tenant] ([TenantId])
 GO
