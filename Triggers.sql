19c5ab189cca34d71a6c702665eb58f92d9307b7
Normal_Pete <petrohorokh@gmail.com>
2024-03-20 13:13:56 +0200

    Added insert and update triggers

---
+++ Triggers.sql
@@ -0,0 +1,395 @@
+CREATE OR ALTER TRIGGER [dbo].[tr_Address_Insert]
+ON [dbo].[Address]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Address]([AddressId],[City],[Street],[Building],[CreatedBy],[CreatedDateTime])
+			SELECT i.[AddressId], i.[City], i.[Street], i.[Building], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted i;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Asset_Insert]
+ON [dbo].[Asset]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Asset]([AssetId],[OwnerId],[RoomId],[CreatedBy],[CreatedDateTime])
+			SELECT [AssetId], [OwnerId], [RoomId], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Bill_Insert]
+ON [dbo].[Bill]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Bill]([BillId],[TenantId],[AssetId],[BillAmount],[IssueDate],[EndDate],[CreatedBy],[CreatedDateTime])
+			SELECT [BillId],[TenantId],[AssetId],[BillAmount],[IssueDate],[EndDate], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Owner_Insert]
+ON [dbo].[Owner]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Owner]([OwnerId],[Name],[AddressId],[CreatedBy],[CreatedDateTime])
+			SELECT [OwnerId],[Name],[AddressId], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Payment_Insert]
+ON [dbo].[Payment]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Payment]([PaymentId],[TenantId],[BillId],[PaymentDay],[Amount],[CreatedBy],[CreatedDateTime])
+			SELECT [PaymentId],[TenantId],[BillId],[PaymentDay],[Amount], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Rent_Insert]
+ON [dbo].[Rent]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Rent]([RentId],[AssetId],[TenantId],[StartDate],[EndDate],[CreatedBy],[CreatedDateTime])
+			SELECT [RentId],[AssetId],[TenantId],[StartDate],[EndDate], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Room_Insert]
+ON [dbo].[Room]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Room]([RoomId],[Number],[Area],[RoomTypeId],[CreatedBy],[CreatedDateTime])
+			SELECT [RoomId],[Number],[Area],[RoomTypeId], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_RoomType_Insert]
+ON [dbo].[RoomType]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[RoomType]([RoomTypeId],[Name],[CreatedBy],[CreatedDateTime])
+			SELECT [RoomTypeId],[Name], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Tenant_Insert]
+ON [dbo].[Tenant]
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			INSERT INTO [dbo].[Tenant]([TenantId],[Name],[BankName],[AddressId],[Director],[Description],[CreatedBy],[CreatedDateTime])
+			SELECT [TenantId],[Name],[BankName],[AddressId],[Director],[Description], '00000000-0000-0000-0000-000000000000' AS [CreatedBy], GETDATE()  AS [CreatedDateTime]
+			FROM inserted;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Address_Update]
+ON [dbo].[Address]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Address]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [AddressId] = (SELECT [AddressId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Asset_Update]
+ON [dbo].[Asset]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Asset]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [AssetId] = (SELECT [AssetId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Bill_Update]
+ON [dbo].[Bill]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Bill]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [BillId] = (SELECT [BillId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Owner_Update]
+ON [dbo].[Owner]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Owner]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [OwnerId] = (SELECT [OwnerId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Payment_Update]
+ON [dbo].[Payment]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Payment]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [PaymentId] = (SELECT [PaymentId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Rent_Update]
+ON [dbo].[Rent]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Rent]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [RentId] = (SELECT [RentId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Room_Update]
+ON [dbo].[Room]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Room]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [RoomId] = (SELECT [RoomId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_RoomType_Update]
+ON [dbo].[RoomType]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[RoomType]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [RoomTypeId] = (SELECT [RoomTypeId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
+
+CREATE OR ALTER TRIGGER [dbo].[tr_Tenant_Update]
+ON [dbo].[Tenant]
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY;
+	    BEGIN TRANSACTION;
+			UPDATE [dbo].[Tenant]
+			SET [ModifiedBy] = '00000000-0000-0000-0000-000000000001', [ModifiedDateTime] = GETDATE()
+			WHERE [TenantId] = (SELECT [TenantId] FROM inserted)
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+        IF @@TRANCOUNT > 0
+			DECLARE @Message [nvarchar](100) = 'An error occurred: ' + ERROR_MESSAGE()
+            RAISERROR( @Message , 11, 0);
+            ROLLBACK TRANSACTION;
+    END CATCH;
+END;
+GO
